382a054d39dd442034fb3b02eeb5a19c185af701
yasu <yasusi0117@live.jp>
2021-08-04 17:06:05 +0900

    業務ロジックの作成

--- src/views/product/SalesRegistration.vue
+++ src/views/product/SalesRegistration.vue
@@ -1,74 +1,276 @@
 <template>
-  <div>
-    <h4>売上登録</h4>
-    <div>
-      フォーム
+    <div class="sales-top">
+      <h4>売上登録</h4>
+      <div class="sales-front">
+        <div class="form">
+          <div class="form-inner">
+            <div class="form-item">
+              <label class="form-item-label" for="category">カテゴリー:</label>
+              <select id="gategory" v-model="search.categoryId" class="form-item-select">
+                <option :value="0">指定なし</option>
+                <option
+                  v-for="category in categories"
+                  :key="category.categoryId"
+                  :value="category.categoryId"
+                  >{{ category.categoryName }}</option
+                >
+              </select>
+            </div>
+
+            <div class="form-item">
+              <label class="form-item-label" for="inStock">在庫数:</label>
+              <input
+                type="radio"
+                id="inStock"
+                value="inStock"
+                v-model="search.stock"
+              /><label class="form-item-label" for="inStock">在庫有</label>
+              <input
+                type="radio"
+                id="outStock"
+                value="outStock"
+                v-model="search.stock"
+              /><label class="form-item-label" for="outStock">在庫無含む</label>
+            </div>
+
+            <div class="form-item">
+              <label class="form-item-label" for="category">商品名:</label>
+              <input type="text" class="form-item-input" v-model="search.productName"/>
+            </div>
+            
+            <div class="form-item">
+              <el-button
+                  type="primary"
+                  icon="el-icon-goods"
+                  size="midium"
+                  plain
+                  @click="hiddenClass"
+                  >一括注文</el-button
+                >
+            </div>
+          </div>
+        </div>
+        <table class="table">
+          <thead class="table-thead">
+            <tr class="table-row-head">
+              <th class="table-th">カテゴリー</th>
+              <th class="table-th">製品名</th>
+              <th class="table-th">売上数</th>
+              <th class="table-th">登録</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- <tr v-for="product in searchProduct" :key="product.productId"> -->
+            <tr v-for="product in partData" :key="product.productId">
+              <td class="table-td">{{ product.category }}</td>
+              <td class="table-td">{{ product.productName }}</td>
+              <td class="table-td">
+                  <!-- @keyup="test(product.buyCount, 'keyup')" -->
+                  <!-- :ref="product.productId" -->
+                <input
+                  class="input-number"
+                  type="number"
+                  min="0"
+                  :max="product.stock"
+                  v-model.number="product.buyCount"
+                  @input="buyCountChecker(product)"
+                />
+                
+              </td>
+              <td class="table-td">
+                <!-- security実装後はorderRequestMethodにユーザーID(employeeId)を含める必要あり -->
+                <el-button
+                  type="primary"
+                  icon="el-icon-goods"
+                  size="midium"
+                  plain
+                  @click="
+                    orderRequest({orderData: {
+                      productId: product.productId,
+                      soldCount: product.buyCount,
+                      stock: product.stock
+                    }, message, productName: product.productName})
+                  "
+                  >注文</el-button
+                >
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div>
+            <!-- @size-change="handleSizeChange" -->
+            <!-- total:全ページ数 page-size:表示させたい要素数  -->
+          <el-pagination
+            background
+            @current-change="handleCurrentChange"
+            :current-page.sync="currentPage"
+            :page-size="displayCount"
+            layout="prev, pager, next, jumper"
+            :total="dataSize"
+            class="pagination"
+            :hide-on-single-page="true"
+          >
+          </el-pagination>
+        </div>
+        <!-- <p>productSize : {{ simpleProductSize }}</p>
+        <p>products: {{ simpleProducts }}</p>
+        <p>search: {{ search }} </p>
+        <p>searchProduct: {{ searchProduct }}</p> -->
+        <p>dataSize: {{ dataSize }}, displayCount: {{ displayCount }}, partData: {{ partData }}</p>
+        <p>{{ currentPage }}</p>
+        <p>{{ withOrderData }}</p>
+      </div>
+
+        <!-- 一括注文で使う場所-->
+        <div class="inner" :class="{active: isActive}" @click.self="hiddenClass">
+          <div class="inner-element">
+            <table class="table inner-element-table">
+              <thead class="table-thead">
+                <tr class="table-row-head inner-element-table-head">
+                  <th class="table-th">カテゴリー</th>
+                  <th class="table-th">製品名</th>
+                  <th class="table-th">売上数</th>
+                </tr>
+              </thead>
+              <tbody>
+                <!-- <tr v-for="product in searchProduct" :key="product.productId"> -->
+                <tr v-for="product in withOrderData" :key="product.productId">
+                  <td class="table-td">{{ product.category }}</td>
+                  <td class="table-td">{{ product.productName }}</td>
+                  <td class="table-td">{{ product.buyCount }}</td>
+                </tr>
+              </tbody>
+            </table>
+             <el-button
+                  type="primary"
+                  icon="el-icon-goods"
+                  size="midium"
+                  plain
+                  class="inner-element-button"
+                  :loading="loading"
+                  @click="multiOrder"
+                  v-if="withOrderData.length > 0"
+                  >一括注文</el-button>
+                  <!-- @click="multipleOrderRequest({requestData: withOrderData, message})" -->
+          </div>
+        </div>
     </div>
-    <table class="table">
-      <thead class="table-thead">
-        <tr class="table-row-head">
-          <th class="table-th">カテゴリー</th>
-          <th class="table-th">製品名</th>
-          <th class="table-th">売上数</th>
-          <th class="table-th">登録</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="product in simpleProducts" :key="product.productId">
-          <td>{{ product.category }}</td>
-          <td>{{ product.productName }}</td>
-          <td>
-            <input type="number" min="0" :max="product.stock" v-model.number="product.count">
-          </td>
-          <td>
-            <!-- security実装後はorderRequestMethodにユーザーID(employeeId)を含める必要あり -->
-            <el-button type="primary" icon="el-icon-goods" size="midium" plain
-            @click="orderRequest({ productId: product.productId, soldCount: product.count , stock: product.stock})">確定</el-button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <div>
-      ページング機能追加
-    </div>
-    <p>productSize : {{ simpleProductSize }}</p>
-    <p>products: {{ simpleProducts }}</p>
-  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import { mapActions } from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "sales",
-  data(){
-    return{
-      m: 5
-    }
+  data() {
+    return {
+      search: {
+        categoryId: 0,
+        stock: "inStock",
+        productName: "",
+      },
+      displayCount: 10, // 1ページ当たりの表示件数
+      currentPage: 1, // 現在のページ
+      isActive: false, // 一括注文用の表示・非表示用トグル
+      withOrderData:[], // 一括注文用データ
+      loading: false // 一括処理用のボタンの制御用
+    };
   },
   computed: {
-    ...mapGetters(["simpleProducts", "simpleProductSize"]),
+    ...mapGetters(["simpleProducts", "simpleProductSize", "categories"]),
+    searchProduct() {
+      return this.$store.getters.searchProduct(this.search);
+    },
+    // searchProductのデータ数
+    dataSize() {
+      return this.searchProduct.length;
+    },
+    // 表示するデータ
+    partData(){
+      const partStartIndex = (this.currentPage - 1) * this.displayCount 
+      const partEndIndex = partStartIndex + this.displayCount
+      return this.searchProduct.slice(partStartIndex, partEndIndex)
+    },
+    message(){
+      return this.$message
+    }
   },
-  methods:{
-    ...mapActions(['orderRequest']),
-  }
+  methods: {
+    ...mapActions(["orderRequest", "multipleOrderRequest"]),
+    handleCurrentChange(val) {
+        console.log(`current page: ${val}`);
+    },
+    // input type number部分のバリデーション
+    buyCountChecker(product){
+      if(product.buyCount < 0){
+        product.buyCount = 0
+        return
+      }else if(product.buyCount > product.stock){
+        product.buyCount = product.stock
+      }
+    },
+    // 一括処理用データの更新処理
+    withOrderDataCounter(){
+      console.log('now counting')
+      this.withOrderData = this.searchProduct.filter(product => product.buyCount > 0)
+    },
+    // 一括処理用のHTMLの表示のON・OFFを切り替える処理と、条件によって一括データを更新する処理
+    hiddenClass(event){
+      // console.log(event)
+      if(event !== undefined && event.target.className != 'inner'){
+        this.withOrderDataCounter()
+      }
+      return this.isActive = !this.isActive
+    },
+    // 一括注文メソッド
+    async multiOrder(){
+      // ボタンを続けて押せないように制御
+      this.loading = true
+      // リクエスト送信
+      let result = await this.multipleOrderRequest({ requestData: this.withOrderData, message: this.message })
+      console.log(result)
+        // ボタンロック解除
+        this.hiddenClass()
+        this.loading = false
+
+      if(result){
+        // 処理が成功した
+      }else{
+        // 処理が失敗した
+        // 失敗以後に購入用データ情報(this.withOrderData)が初期化されてしまう為、直されたデータを元に購入数データを書き換えている
+        this.searchProduct.forEach(product =>{
+          this.withOrderData.forEach(buyData =>{
+            if(product.productId == buyData.productId){
+              product.buyCount = product.buyCount < buyData.buyCount ? product.stock : buyData.buyCount
+            }
+          })
+        })
+      }
+    }
+  },
+   //ページが読み込まれる前に発火(this利用不可)
+    // beforeRouteEnter(to, from, next){
+    //     console.log('enter')
+    //     console.log(to)
+    //     next({query:{q1: "q1"}})
+    // },
+    //     // ページが更新されたときに発火
+    // beforeRouteUpdate(to, from, next){
+    //     console.log('update')
+    //     next()
+    // },
+    //     // ページから離れたとき発火
+    // beforeRouteLeave(to, from, next){
+    //     console.log('leave')
+    //         next()
+    // },
 };
 </script>
 
 <style scoped lang="scss">
-.table {
-  width: 100%;
-  border-spacing: 0;
-  &-th {
-    border-bottom: 4px solid #dddddd;
-  }
-  &-td {
-    border-bottom: 2px solid #dddddd;
-  }
+@import '../../style/show_table.scss';
+@import '../../style/inner_table.scss';
+@import '../../style/search_form.scss';
+@import '../../style/pagination.scss';
 
-  & tbody td {
-    padding: 15px 0;
-  }
-}
 </style>
--- src/views/sales/SalesConfirmation.vue
+++ src/views/sales/SalesConfirmation.vue
@@ -1,6 +1,69 @@
 <template>
   <div>
     <h4>売上</h4>
+    <div class="form">
+      <div class="form-inner">
+        <div class="form-item">
+          <label class="form-item-label" for="category">カテゴリー:</label>
+          <select id="cgategory" v-model="search.categoryId" class="form-item-select">
+            <option :value="0">指定なし</option>
+            <option
+              v-for="category in categories"
+              :key="category.categoryId"
+              :value="category.categoryId"
+              >{{ category.categoryName }}</option
+            >
+          </select>
+        </div>
+
+        <div class="form-item">
+          <label class="form-item-label" for="dates">カテゴリー:</label>
+          <select id="dates" v-model="search.specifyDate" class="form-item-select">
+            <option
+              v-for="date in dates"
+              :key="date.id"
+              :value="date.id"
+              >{{ date.date }}</option
+            >
+          </select>
+        </div>
+
+        <!-- <div class="form-item">
+          <span>日付指定:</span>
+          <input
+            type="radio"
+            id="thisMonth"
+            value="thisMonth"
+            v-model="search.specifyDate"
+          /><label class="form-item-label" for="thisMonth">今月</label>
+          <input
+            type="radio"
+            id="thisWeek"
+            value="thisWeek"
+            v-model="search.specifyDate"
+          /><label for="thisWeek">今週</label>
+          <input
+            type="radio"
+            id="today"
+            value="today"
+            v-model="search.specifyDate"
+          /><label for="today">今日</label>
+          <input
+            type="radio"
+            id="all"
+            value="all"
+            v-model="search.specifyDate"
+          /><label for="all">全て</label>
+        </div> -->
+
+        <div class="form-item">
+          <label for="category" class="form-item-label">商品名:</label>
+          <!-- v-model.lazyを使うか検討 -->
+          <input type="text" v-model="search.productName" class="form-item-input"/>
+        </div>
+      </div>
+    </div>
+
     <table class="table">
       <thead class="table-thead">
         <tr class="table-row-head">
@@ -12,7 +75,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="sale in simpleSales" :key="sale.salesId">
+        <tr v-for="sale in partData" :key="sale.salesId">
           <td class="table-td">{{ replaceDay(sale.salesTime) }}</td>
           <td class="table-td">{{ sale.category }}</td>
           <td class="table-td">{{ sale.productName }}</td>
@@ -21,49 +84,88 @@
         </tr>
       </tbody>
     </table>
-    <!-- 下記の場合nは1からスタート、simpleSalesの要素がない場合にはエラーになるので
-    全要素数を事前に知っておく必要あり -->
-    <p v-for="n in 9" :key="n">{{ simpleSales[n - 1].salesTime }} : {{ simpleSales[n - 1].category }} : {{ simpleSales[n - 1].parseDate }}</p>
+    <div>
+       <!-- @size-change="handleSizeChange" -->
+        <!-- total:全ページ数 page-size:表示させたい要素数  -->
+      <el-pagination
+        background
+        @current-change="handleCurrentChange"
+        :current-page.sync="currentPage"
+        :page-size="displayCount"
+        layout="prev, pager, next, jumper"
+        :total="dataSize"
+        class="pagination"
+        :hide-on-single-page="true"
+      >
+      </el-pagination>
+    </div>
+
     <p>
-      <!-- {{ sales }} -->
+      {{ search }}
     </p>
     <p>
       <!-- {{ simpleSales }} -->
     </p>
+    <div>
+      {{ simpleOrderDateSales }}
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+// import { sortObj } from '../../utils/utils'
 
 export default {
   name: "confirmation",
+  data() {
+    return {
+      search: {
+        categoryId: 0,
+        specifyDate: "thisMonth",
+        productName: "",
+      },
+      dates:{
+        thisMonth: { id: 'thisMonth', date: '今月'},
+        thisWeek: { id: 'thisWeek', date: '今週'},
+        today: { id: 'today', date: '今日'},
+        thisYear: { id: 'thisYear', date: '今年'},
+        all: { id: 'all', date: '全て'},
+      },
+      displayCount: 10, // 1ページ当たりの表示件数
+      currentPage: 1 // 現在のページ
+    };
+  },
   computed:{
-    ...mapGetters(["sales","simpleSales"]),
+    ...mapGetters(["sales", "categories"]),
+    simpleOrderDateSales(){
+      return this.$store.getters.simpleOrderDateSales(this.search)
+    },
+      // simpleOrderDateSalesのデータ数
+    dataSize() {
+      return this.simpleOrderDateSales.length;
+    },
+    // 表示するデータ
+    partData(){
+      const partStartIndex = (this.currentPage - 1) * this.displayCount 
+      const partEndIndex = partStartIndex + this.displayCount
+      return this.simpleOrderDateSales.slice(partStartIndex, partEndIndex)
+    }
   },
   methods:{
     replaceDay(dayObj){
       return dayObj.replace(/-/,'年').replace(/-/,'月').replace(/T/,'日 ');
-    }
+    },
+    handleCurrentChange(val) {
+        console.log(`current page: ${val}`);
+    },
   }
 };
 </script>
 
 <style scoped lang="scss">
-  .table{
-    width: 100%;
-    border-spacing: 0;
-    &-th{
-      border-bottom: 4px solid #DDDDDD;
-    }
-    &-td{
-      border-bottom: 2px solid #DDDDDD;
-    }
-    
-    & tbody td{
-      padding: 15px 0;
-    }
-  }
-
+@import '../../style/search_form.scss';
+@import '../../style/show_table.scss';
+@import '../../style/pagination.scss';
 
 </style>
