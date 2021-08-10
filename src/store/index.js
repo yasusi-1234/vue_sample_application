6d644ba2a55519c19d7d0939290c14c0001b3ca8
yasu <yasusi0117@live.jp>
2021-08-11 03:03:28 +0900

    セキュリティー用の処理実装
    コードリファクタリング

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
+import ElementUI from 'element-ui'
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from '../communication/communication'
+import router from '../router/router'
+// import rawAxios from 'axios'
 
 import { sortObj, sortObjReverse, betweenSpecifyDate } from '../utils/utils'
 
@@ -11,11 +14,13 @@ export default new Vuex.Store({
     sales: [],
     products: [],
     categories: [],
-    isLogin: false, // テスト用
+    accessToken: null, // エンドポイントアクセス用トークン
   },
   getters:{
+    // アクセストークン
+    headerInAccessToken: state => {return {headers: {'Authorization': state.accessToken}}},
     // テスト用 loginしているかのフラグ
-    isLogin: state => state.isLogin,
+    isLogin: state => state.accessToken !== null,
     // 製品販売情報のリスト
     sales: state => state.sales,
     // 製品データ
@@ -49,10 +54,8 @@ export default new Vuex.Store({
     },
     // 製品情報から条件で絞って抽出したデータリスト
     searchProduct: state => ({categoryId, stock, productName}) => {
-      // const data = getters.products.map(product =>({...product}))
       const searchName = productName.trim()
       const noText = searchName === ''
-      // console.log(data, categoryId, stock);
 
       const returnData = state.products.filter(d => { 
         
@@ -94,16 +97,19 @@ export default new Vuex.Store({
     categories: state => state.categories,
   },
   mutations: {
-    // ログイン情報の変更
-    loginChange(state, status){
-      state.isLogin = status
+    // アクセストークンセット
+    setAccessToken(state, token){
+      state.accessToken = token;
     },
+    // 販売履歴データをstateにセット
     setSales(state, salesData){
       state.sales = salesData
     },
+    // 商品状態をstateにセット
     setProducts(state, productsData){
       state.products = productsData 
     },
+    // 商品カテゴリー情報をstateにセット
     setCategories(state, categoryData){
       state.categories = categoryData
     },
@@ -122,9 +128,11 @@ export default new Vuex.Store({
       }
       state.sales.push(sale)
     },
+    // 製品情報をステートに追加
     addProduct(state, requestProduct){
       state.products.push(requestProduct)
     },
+    // 製品カテゴリーをstateに追加
     addCategory(state, categoryData){
       state.categories.push(categoryData)
     },
@@ -135,7 +143,6 @@ export default new Vuex.Store({
       let product = products.find(p => p.productId == requestData.product.productId)
       // stateのproductsの内部情報更新
       product.stock = requestData.product.stock
-      console.log(error)
       if(error){
         // エラーだった場合のリクエスト
         product.buyCount = requestData.product.stock
@@ -155,21 +162,26 @@ export default new Vuex.Store({
     },
     // 製品販売情報の要素をIdから特定し削除する
     deleteSalesData(state, deleteId){
-      console.log(deleteId)
       state.sales = state.sales.filter(sale => sale.salesId !== deleteId)
     },
+    // ログアウト処理,データの初期化を行う
+    logout(state){
+      state.sales = []
+      state.products = []
+      state.categories = []
+      state.accessToken = null
+    }
   },
   actions: {
-    loginChange({ commit }, inputData){
-      // 本来はここでログイン情報が正しいかチェックし、エンドポイントにアクセスしてその返り値に応じて適切に処理をする
-      console.log(inputData)
-
-      if(inputData === false){
-        commit('loginChange', false) // test用
-      }else{
-        commit('loginChange', true) // test用
-      }
+    // アクセストークンのセット
+    setAccessToken({ commit }, token){
+      commit('setAccessToken', token)
     },
+    // ログアウト処理
+    logout({ commit }){
+      commit('logout')
+    },
+    // 渡されたデータをもとに扱いやすい状態に変換し、stateにセット
     setSales({ commit }, salesData){
       const sales = salesData.map(sale =>{
         return {
@@ -186,7 +198,7 @@ export default new Vuex.Store({
       })
       commit('setSales', sales)
     },
-    // 製品情報
+    // 製品情報を扱いやすい情報に変換し、stateにセット
     setProducts({ commit }, productData){
       const products = productData.map(product =>{
         return {
@@ -202,16 +214,17 @@ export default new Vuex.Store({
       })
       commit('setProducts', products)
     },
+    // カテゴリー情報をstateにセット
     setCategories({ commit }, categoryData){
       sortObj(categoryData, 'categoryId');
       commit('setCategories', categoryData)
     },
     // 販売完了ボタン(salesRegistration.vueで使われるメソッド)
-    orderRequest( { commit } , {orderData, message, productName}){
+    orderRequest( { commit, getters, dispatch } , {orderData, productName}){
       // 処理:販売情報に問題無い事を確認した上でを確認しサーバー側にアップデートリクエストを投げる
       if(orderData.soldCount <= 0){
         // リクエストの値が0以下の場合
-        message({
+        ElementUI.Message({
           message: "値が0になっています。値を入力してください",
           type: "warning"
         })
@@ -219,7 +232,7 @@ export default new Vuex.Store({
       }
       if(orderData.stock < orderData.soldCount){
         // 在庫数を超えたリクエストがされた場合
-        message({
+        ElementUI.Message({
           message: "不正な値が入力されています。修正してください。現在の在庫数：" + orderData.stock + " リクエストされた在庫数： " + orderData.soldCount,
           type: "error",
           duration: 5000,
@@ -232,72 +245,72 @@ export default new Vuex.Store({
       const {soldCount, productId} = orderData
       const requestData = {soldCount, productId, employeeId}
       // postRequest
-      axios.post('/sales/register',requestData).then(res => {
-        console.log(res.data)
+      axios.post('/sales/register',requestData, getters.headerInAccessToken).then(res => {
         commit('addSales', res.data)
         commit('updateProducts', { requestData: res.data })
-        message({
+        ElementUI.Message({
           message: "販売リクエストが完了しました。販売商品： " + productName + " 販売数： " + soldCount,
           type: "success"
         })
       }).catch(error =>{
-        console.log(error.response)
+        // console.log(error.response)
         // 商品情報のズレがある場合の返却値が含まれていれば
         if(error.response.data.product){
 
           commit('updateProducts', { requestData: error.response.data, error: true });
-          message({
+          ElementUI.Message({
             message: "在庫数にズレがあったため、販売リクエストが失敗しました。実在庫数： " +
             error.response.data.product.stock + " リクエスト販売数： " + soldCount + " 値を修正してください。",
             type: "warning"
           })
-          
-        }else{
-          // 予期しないエラー
-          message({
-            message: error,
-            type: error
-          })
+          return
         }
+        
+        dispatch('errorHandling', error)
 
       })
     },
+    // 製品情報を整形した上で既存のProduct情報に追加
     addProduct({ commit }, productData){
-      commit('addProduct', productData);
+      const product = {
+        category: productData.category.categoryName,
+        categoryId: productData.category.categoryId,
+        productName: productData.productName,
+        price: productData.price,
+        productId: productData.productId,
+        stock: productData.stock,
+        buyCount: 0,
+        supplyCount: 0
+      }
+      commit('addProduct', product);
     },
     addCategory({ commit }, categoryData){
       commit('addCategory', categoryData)
     },
     // 商品の補充を受け付けるメソッド
-    supplyProduct({ commit }, {requestData, message, productName}){
+    supplyProduct({ commit, getters, dispatch }, {requestData, productName}){
       if(requestData.requestValue <= 0){
-        message({
+        ElementUI.Message({
           message: "供給量が0になっています。値を修正してください。",
           type: "warning"
         })
         return
       }
-      axios.patch('/product/update',requestData).then(res =>{
-        console.log(res.data)
+      axios.patch('/product/update', requestData, getters.headerInAccessToken).then(res =>{
         commit('updateSupplyProducts', res.data)
-        message({
+        ElementUI.Message({
           message: "商品： " + productName + "を" + requestData.requestValue + "個補充しました",
           type: "success",
         });
       }).catch(error =>{
-        console.log(error)
-        message({
-          message: error,
-          type: "error",
-          duration: 5000,
-        });
+        dispatch('errorHandling', error)
       })
     },
     // 複数の注文を受け付けるメソッド
-    multipleOrderRequest({ commit }, { requestData, message }){
+    multipleOrderRequest({ commit, getters, dispatch }, { requestData }){
       if(requestData.length === 0){
         // リクエストデータが空の場合
-        message({
+        ElementUI.Message({
           type: 'error',
           message: 'リクエストデータがありません。処理を中断しました'
         })
@@ -312,17 +325,16 @@ export default new Vuex.Store({
           soldCount: data.buyCount
         }
       });
-      console.log(salesForms)
+      
       const ListSalesForm = {salesForms}
-      const result = axios.post('/sales/multiple/register', ListSalesForm).then( res =>{
-        console.log(res.data)
+      const result = axios.post('/sales/multiple/register', ListSalesForm, getters.headerInAccessToken).then( res =>{
         res.data.forEach(data =>{
           // 販売情報の追加
           commit('addSales', data)
           // 商品状態の更新
           commit('updateProducts', { requestData: data })
         })
-        message({
+        ElementUI.Message({
           message: '販売情報の更新が完了しました',
           type: 'success'
         })
@@ -331,31 +343,25 @@ export default new Vuex.Store({
         return true
       }).catch(error => {
         // 商品情報のズレがある場合の返却値が含まれていれば ただし現状は一つの要素しか返ってこない
-        console.log(error.response)
         if(error.response.data.product){
           commit('updateProducts', { requestData: error.response.data, error: true})
-          message({
+          ElementUI.Message({
             message: error.response.data.product.productName + "の在庫数にズレがあったため、販売リクエストが失敗しました。実在庫数： " +
             error.response.data.product.stock + " 値を修正してください。",
             type: "warning"
           })
           return false
-        }else{
-          // 予期しないエラー
-          message({
-            message: error,
-            type: error
-          })
-          return false
         }
+
+        return dispatch('errorHandling', error)
       })
       // 戻り値 成功でtrue 失敗でfalse
       return result
     },
-    multipleSupplyRequest({ commit }, { requestData, message }){
+    multipleSupplyRequest({ commit, getters, dispatch }, { requestData }){
       if(requestData.length === 0){
         // リクエストデータが空の場合
-        message({
+        ElementUI.Message({
           type: 'error',
           message: 'リクエストデータがありません。処理を中断しました'
         })
@@ -364,42 +370,36 @@ export default new Vuex.Store({
       const updateProducts = requestData.map(data => {
         return {productId: data.productId, requestValue: data.supplyCount, subtraction: false}
       });
-      console.log(updateProducts)
       const updateProductsForm = { updateProducts }
 
-      const result = axios.patch('/product/multiple/update', updateProductsForm).then(res =>{
-        console.log(res.data);
+      const result = axios.patch('/product/multiple/update', updateProductsForm, getters.headerInAccessToken).then(res =>{
         res.data.forEach(data => commit('updateSupplyProducts', data))
         
-        message({
+        ElementUI.Message({
           type: 'success',
           message: '商品の補充処理が成功しました'
         })
         return true
       }).catch(error => {
         // 失敗したときの処理
-        message({
-          type: 'warning',
-          message: error
-        })
-        return false
+        return dispatch('errorHandling', error)
       })
 
       return result
     },
     // 注文情報の削除(取り消し処理)
-    async deleteSalesRequest({ commit }, { deleteData, message, confirm}){
+    async deleteSalesRequest({ commit, getters, dispatch }, { deleteData, confirm}){
       
+      // ウィンドウに本当に削除指定良いかのWindowを表示する処理
       let isRequestOk = await confirm(deleteData.length + '件のデータを削除します。よろしいですか？', '注文情報の削除',{
         confirmButtonText: '削除確定',
         cancelButtonText: '取り消し',
         type: 'warning',
         center: true
       }).then(() => {
-        console.log('YES')
         return true
       }).catch(() => {
-        message({
+        ElementUI.Message({
           type: 'info',
           message: '処理をキャンセルしました',
           center: true,
@@ -411,15 +411,13 @@ export default new Vuex.Store({
       if(isRequestOk){
         if(deleteData.length === 1){
           const deleteId = deleteData[0]
-          console.log('処理前')
           // 要素が一つの場合
-          const result = await axios.delete('sales/delete', {data: deleteId}).then(res => {
-            console.log(res.data)
+          const result = await axios.delete('sales/delete', {data: deleteId, ...getters.headerInAccessToken}, ).then(res => {
             // 注文情報の削除
             commit('deleteSalesData', deleteData[0])
             // 製品情報の更新
             commit('updateSupplyProducts', res.data)
-            message({
+            ElementUI.Message({
               type: 'success',
               message: '注文情報の削除処理及び商品情報の更新に成功しました',
               duration: 5000,
@@ -427,29 +425,20 @@ export default new Vuex.Store({
             })
             return true
           }).catch(error => {
-            // ほぼ起こらないと思われる
-            message({
-              type: 'error',
-              duration: 5000,
-              message: error,
-              center: true
-            })
-            return false
+            return dispatch('errorHandling', error)
           })
 
           return result
         }else{
           // 要素が複数の場合
-          const result = await axios.delete('/sales/multipleDelete', {data: deleteData}).then(res =>{
-            console.log(res.data)
+          const result = await axios.delete('/sales/multipleDelete', {data: deleteData, ...getters.headerInAccessToken}, ).then(res =>{
             res.data.forEach((product, index) =>{
-              console.log(product, index)
               // 注文情報の削除
               commit('deleteSalesData', deleteData[index])
               // 製品情報の更新
               commit('updateSupplyProducts', product)
             })
-            message({
+            ElementUI.Message({
               type: 'success',
               message: '注文情報の削除処理及び商品情報の更新に成功しました',
               duration: 5000,
@@ -458,18 +447,79 @@ export default new Vuex.Store({
             return true
           }).catch(error => {
              // ほぼ起こらないと思われる
-             message({
-              type: 'error',
-              duration: 5000,
-              message: error,
-              center: true
-            })
-            return false
+             return dispatch('errorHandling', error)
           })
           return result
         }
       }
       return isRequestOk
+    },
+    /** 新規に商品情報を登録する処理 */
+    async createProduct({ getters, dispatch }, { addProductData }){
+      console.log(addProductData)
+      // 登録リクエスト
+      const result = await axios
+      .post("/product/register", addProductData, getters.headerInAccessToken)
+      .then((res) => {
+        dispatch('addProduct', res.data)
+        ElementUI.Message({
+          message: "新しい商品を追加しました",
+          type: "success",
+        })
+        return true
+      })
+      .catch((error) => {
+        // 失敗した場合の処理
+         return dispatch('errorHandling', error)
+      });
+
+      return result
+    },
+    /** 新規にカテゴリー情報を登録する処理 */
+    async createCategory({ commit, getters, dispatch }, { categoryName }){
+      const result = await axios
+          .post("/product/category/register", {categoryName}, getters.headerInAccessToken)
+          .then((res) => {
+            commit('addCategory', res.data)
+            ElementUI.Message({
+              message: "新しいカテゴリーを追加しました",
+              type: "success",
+            })
+            return true
+          })
+          .catch((error) => {
+            // 失敗した場合の処理
+            return dispatch('errorHandling', error)
+          });
+
+      return result
+    },
+    /** エンドポイントからのレスポンスでエラーが返ってきた際の共通処理。主に
+     * ログインタイムアウトの場合の処理、予測不能のエラーの処理
+     */
+    errorHandling({ commit }, error){
+      console.log('call error handling')
+      if(error.response.headers['token-expired']){
+        // トークンの期限切れの場合
+        ElementUI.Message({
+          message: 'ログイン有効時間が過ぎました',
+          type: "error",
+          duration: 5000,
+        })
+        // 初期化処理
+        commit('logout')
+        // 強制的にログイン画面へ遷移
+        router.push('/login')
+        return false
+      }else{
+        console.log('error other')
+        ElementUI.Message({
+          message: error,
+          type: "error",
+          duration: 5000,
+        })
+        return false
+      }
     },
   },
   modules: {
