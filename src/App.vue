dba99c0df7841b4d4078a1f6a52d05428aaed89c
yasu <yasusi0117@live.jp>
2021-08-11 03:06:43 +0900

    使わなくなった部分のコメント化

--- src/App.vue
+++ src/App.vue
@@ -7,36 +7,36 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import axios from './communication/communication'
+// import { mapActions } from 'vuex'
+// import axios from './communication/communication'
 // test用
 console.log('hello vue.js')
 export default{
   name: "app",
-  data(){
-    return {
-      access: true,
-    }
-  },
-  methods:{
-    ...mapActions(["setSales", "setProducts", "setCategories"])
-  },
+  // data(){
+  //   return {
+  //     access: true,
+  //   }
+  // },
+  // methods:{
+  //   ...mapActions(["setSales", "setProducts", "setCategories"])
+  // },
   // test段階の実装、後にログインしてから読み込みするように変更する。また数が多い場合も考慮して
   // 分割してエンドポイントにアクセスするかも考える必要あり
-  created(){
-    if(this.access){
-      axios.get('sales').then(res => {
-        console.log('sales data dawnloaded')
-        this.setSales(res.data)
-      })
-      axios.get('product').then(res =>{
-        this.setProducts(res.data)
-      })
-      axios.get('product/category').then(res =>{
-        this.setCategories(res.data);
-      })
-    }
-  }
+  // created(){
+    // if(this.access){
+    //   axios.get('sales').then(res => {
+    //     console.log('sales data dawnloaded')
+    //     this.setSales(res.data)
+    //   })
+    //   axios.get('product').then(res =>{
+    //     this.setProducts(res.data)
+    //   })
+    //   axios.get('product/category').then(res =>{
+    //     this.setCategories(res.data);
+    //   })
+    // }
+  // }
 }
 </script>
 
